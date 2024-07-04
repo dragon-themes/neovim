d989213a05758a206cfaedd1a295362522c0f02b
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2024-07-03 23:38:52 -0300

    feat: add a new coloscheme called agni based on Gruvbox

--- colors/agni.lua
+++ colors/agni.lua
@@ -1,24 +1,20 @@
 local agni = {
-  black = "#282828",
-  black2 = "#3c3836",
-  black3 = "#504945",
-  white = "#d5c4a1",
-  yellow = "#fabd2f",
-  dark_yellow = "#d79921", -- nao usado
-  orange = "#fe8019",      -- nao usado
-  dark_orange = "#d65d0e",
-  red = "#fb4934",
-  dark_red = "#cc241d",     -- nao usado
-  purple = "#d3869b",
-  dark_purple = "#b16286", -- nao usado
-  blue = "#fe8019",
-  dark_blue = "#458588",    -- nao usado
-  aqua = "#8ec07c",
-  dark_aqua = "#689d6a",    -- nao usado
-  green = "#b8bb26",
-  dark_green = "#98971a", -- nao usado
-  grey = "#a89984",
-  dark_grey = "#7c6f64"
+  black = "#d5c4a1",          --1 foreground
+  red = "#cc241d",            --2 error
+  green = "#d3869b",          --3 text_number
+  yellow = "#665c54",         --4 ui2
+  blue = "#b8bb26",           --5 color_support
+  magenta = "#8ec07c",        --6 text_string
+  cyan = "#a89984",           --7 text-comment, punctuation
+  white = "#282828",          --8 background
+  bright_black = "#504945",   --9 ui1
+  bright_red = "#fb4934",     --10 danger
+  bright_green = "#fabd2f",   --11 color_secondary
+  bright_yellow = "#7c6f64",  --12 warning
+  bright_blue = "#fe8019",    --13 color_primary
+  bright_magenta = "#83a598", --14 color_extra
+  bright_cyan = "#689d6a",    --15 info
+  bright_white = "#458588",   --16 visual
 }
 
 require('dragon-themes').colorscheme('agni',agni)
