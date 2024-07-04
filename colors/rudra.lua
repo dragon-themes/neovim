5bddd6fe31e0f663ed3d3681c9b808d848981aef
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2024-07-03 23:24:24 -0300

    feat: add a new colorscheme called rudra based on Palenight

--- colors/rudra.lua
+++ colors/rudra.lua
@@ -1,51 +1,20 @@
-local rudra2 = {
-  black = "#202d3e",
-  black2 = "#434758",
-  black3 = "#585d81",
-  white = "#d0d0d0",
-  dark_orange = "#82aaff",
-  dark_yellow = "#d79921", -- nao usado
-  green = "#c3e88d",       -- nao usado
-  orange = "#d65d0e",
-  red = "#fb4934",
-  dark_red = "#cc241d",     -- nao usado
-  purple = "#d3869b",
-  dark_primary = "#b16286", -- nao usado
-  blue = "#c792ea",
-  dark_blue = "#458588",    -- nao usado
-  aqua = "#ff8b92",
-  dark_aqua = "#689d6a",    -- nao usado
-  yellow = "#89ddff",
-  dark_green = "#98971a",   -- nao usado
-  grey = "#767ba4",
-  dark_grey = "#7c6f64"
-}
-
 local rudra = {
-  black = "#202d3e",
-  black2 = "#434758",
-  black3 = "#585d81",
-  white = "#d0d0d0",
-  --[[ black = "#202d3e", ]]
-  --[[ black2 = "#434758", ]]
-  --[[ black3 = "#585d81", ]]
-  --[[ white = "#d0d0d0", ]]
-  yellow = "#82aaff",
-  dark_yellow = "#d79921", -- nao usado
-  orange = "#fe8019",      -- nao usado
-  dark_orange = "#d65d0e",
-  red = "#fb4934",
-  dark_red = "#cc241d",     -- nao usado
-  purple = "#d3869b",
-  dark_primary = "#b16286", -- nao usado
-  blue = "#c792ea",
-  dark_blue = "#458588",    -- nao usado
-  aqua = "#c3e88d",
-  dark_aqua = "#689d6a",    -- nao usado
-  green = "#89ddff",
-  dark_green = "#98971a",   -- nao usado
-  grey = "#767ba4",
-  dark_grey = "#7c6f64"
+  black = "#c6cedb",          --1 foreground
+  red = "#cc241d",            --2 error
+  green = "#b16286",          --3 text_number
+  yellow = "#585d81",         --4 ui2
+  blue = "#82d3cb",           --5 color_support
+  magenta = "#c3ed91",        --6 text_string
+  cyan = "#767ba4",           --7 text-comment, punctuation
+  white = "#2f3348",          --8 background
+  bright_black = "#3c3f63",   --9 ui1
+  bright_red = "#fe9273",     --10 danger
+  bright_green = "#90b3ff",   --11 color_secondary
+  bright_yellow = "#feee99",  --12 warning
+  bright_blue = "#d599f0",    --13 color_primary
+  bright_magenta = "#ffca6b", --14 color_extra
+  bright_cyan = "#c3e88d",    --15 info
+  bright_white = "#925dcd",   --16 visual
 }
 
 require('dragon-themes').colorscheme('rudra', rudra)
