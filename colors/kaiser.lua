3c1831ba6159e58d025134338101b271224949d7
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2024-07-03 23:38:10 -0300

    doc: add info of each color

--- colors/kaiser.lua
+++ colors/kaiser.lua
@@ -1,20 +1,20 @@
 local kaiser = {
-  black = "#d5c4a1",          --1
-  red = "#cc241d",            --2 nao usado
-  green = "#d3869b",          --3
-  yellow = "#504945",         --4
-  blue = "#b8bb26",           --5
-  magenta = "#8ec07c",        --6
-  cyan = "#a89984",           --7
-  white = "#282828",          --8
-  bright_black = "#3c3836",   --9
-  bright_red = "#fb4934",     --10
-  bright_green = "#fabd2f",   --11
-  bright_yellow = "#7c6f64",  --12
-  bright_blue = "#83a598",    --13
-  bright_magenta = "#d65d0e", --14 nao usado
-  bright_cyan = "#689d6a",    --15
-  bright_white = "#458588",   --16
+  black = "#d5c4a1",          --1 foreground
+  red = "#cc241d",            --2 error
+  green = "#d3869b",          --3 text_number
+  yellow = "#504945",         --4 ui2
+  blue = "#b8bb26",           --5 color_support
+  magenta = "#8ec07c",        --6 text_string
+  cyan = "#a89984",           --7 text-comment, punctuation
+  white = "#282828",          --8 background
+  bright_black = "#3c3836",   --9 ui1
+  bright_red = "#fb4934",     --10 danger
+  bright_green = "#fabd2f",   --11 color_secondary
+  bright_yellow = "#7c6f64",  --12 warning
+  bright_blue = "#83a598",    --13 color_primary
+  bright_magenta = "#d65d0e", --14 color_extra
+  bright_cyan = "#689d6a",    --15 info
+  bright_white = "#458588",   --16 visual
 }
 
 require('dragon-themes').colorscheme('kaiser', kaiser)
