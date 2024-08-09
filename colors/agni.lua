dbfd1670cf11b797aa6831e180820197ae9a0b43
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2024-08-09 17:01:02 -0300

    feat: update Agni to be similar to One Monekai

--- colors/agni.lua
+++ colors/agni.lua
@@ -1,20 +1,20 @@
 local agni = {
-  black = "#d5c4a1",          --1 foreground
+  black = "#E6DDD0",          --1 foreground
   red = "#cc241d",            --2 error
-  green = "#d3869b",          --3 text_number
-  yellow = "#665c54",         --4 ui2
-  blue = "#b8bb26",           --5 color_support
-  magenta = "#8ec07c",        --6 text_string
-  cyan = "#a89984",           --7 text-comment, punctuation
-  white = "#282828",          --8 background
-  bright_black = "#504945",   --9 ui1
+  green = "#f89868",          --3 text_number
+  yellow = "#413A43",         --4 ui2
+  blue = "#78DCE8",           --5 color_support
+  magenta = "#FCD866",        --6 text_string
+  cyan = "#8A827F",           --7 text-comment, punctuation
+  white = "#2E282F",          --8 background
+  bright_black = "#3B343C",   --9 ui1
   bright_red = "#fb4934",     --10 danger
-  bright_green = "#fabd2f",   --11 color_secondary
-  bright_yellow = "#7c6f64",  --12 warning
-  bright_blue = "#fe8019",    --13 color_primary
-  bright_magenta = "#83a598", --14 color_extra
+  bright_green = "#A9DC76",   --11 color_secondary
+  bright_yellow = "#b9bb26",  --12 warning
+  bright_blue = "#F66189",    --13 color_primary
+  bright_magenta = "#F89868", --14 color_extra
   bright_cyan = "#689d6a",    --15 info
-  bright_white = "#458588",   --16 visual
+  bright_white = "#474246",   --16 visual
 }
 
 require('dragon-themes').colorscheme('agni',agni)
